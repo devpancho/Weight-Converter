38fb488a954011064d12c2c6b28e140f86be0db1
devpancho <udeh.pancho@gmail.com>
2022-09-05 11:18:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('output').style.visibility = 'hidden'
+
+
+document.getElementById('weightInput').addEventListener('input',  function (e) {
+    document.getElementById('output').style.visibility = 'visible'
+
+        let pounds = e.target.value;
+
+        document.getElementById('gramsOutput').innerHTML = pounds/0.0022046;
+
+        document.getElementById('kgOutput').innerHTML = pounds*0.454;
+        
+        document.getElementById('tonOutput').innerHTML = pounds*0.454;
+
+        document.getElementById('mgOutput').innerHTML = pounds*453592;
+
+        document.getElementById('ozOutput').innerHTML = pounds*16;
+    });
